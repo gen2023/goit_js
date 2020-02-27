eddfde59e55984f724478dc3ee83814bfd0274d9
Евгений <evgenij.romanko@neoseo.com.ua>
2020-02-27 21:54:30 +0200

    event enter+ if total==0

--- module13/src/index.js
+++ module13/src/index.js
@@ -16,14 +16,17 @@ const key = '15335166-46184c58e4c8bb577f38781ce';
 
 function urlImage() {
   nameImg = input.value;
-  console.log(key);
   url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${nameImg}&page=${numberPage}&per_page=12&key=${key}`;
 }
 
 function listImage(data) {
-  const name = data.hits.map(data => templateList(data)).join('');
-  gallery.insertAdjacentHTML('beforeend', name);
-  loadMore.hidden = false;
+  if (data.total === 0) {
+    alert('no image');
+  } else {
+    const name = data.hits.map(data => templateList(data)).join('');
+    gallery.insertAdjacentHTML('beforeend', name);
+    loadMore.hidden = false;
+  }
 }
 
 function clearListItems() {
@@ -31,15 +34,26 @@ function clearListItems() {
   loadMore.hidden = true;
 }
 
+function stopEvent(event) {
+  input.addEventListener('keydown', function(event) {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+    }
+  });
+}
+
 function outputText(event) {
+  stopEvent();
   urlImage();
-  fetch(url + nameImg)
+  fetch(url)
     .then(response => {
+      // console.log(response.json());
       return response.json();
     })
     .then(data => {
       if (nameImg === '') {
         clearListItems();
+        stopEvent();
       } else {
         listImage(data);
       }
